c7d94cf6332d89252358edb943e7911ee16f3381
trizin <zates7624@gmail.com>
2020-09-16 17:50:12 +0300

    com

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Easy Json Translate
+Translates every string in json using google cloud translation api.
+## Setup
+---
+You need to setup the api key for google cloud  
+Follow the instructions here https://cloud.google.com/translate/docs/setup
+## Usage
+---
+
--- translate.js
+++ translate.js
@@ -1,17 +1,31 @@
 let fs = require("fs");
-const { type } = require("os");
+
+const { Translate } = require("@google-cloud/translate").v2;
+const translate = new Translate();
 let data = fs.readFileSync("./data/data_en.json");
 data = JSON.parse(data);
+const target = "ru";
 
-getDeeper(data);
-
-function getDeeper(obj) {
+async function getDeeper(obj) {
+  //Gets deeper...
   for (var k of Object.keys(obj)) {
     if (typeof obj[k] == "object") {
       getDeeper(obj[k]);
     } else if (typeof obj[k] == "string") {
       // Found string translate here
-      obj[k] = "RANDOMVALUE";
+      obj[k] = await translateText(obj[k], target);
     }
   }
+  fs.writeFileSync("./translated.json", JSON.stringify(data));
 }
+
+async function translateText(text, target) {
+  // Translates text to target language using google cloud api
+  let [translations] = await translate.translate(text, target);
+  translations = Array.isArray(translations) ? translations : [translations];
+  let translation = translations[0];
+  console.log(translation);
+  return translation;
+}
+
+getDeeper(data);
